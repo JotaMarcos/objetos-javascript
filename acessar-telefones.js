75c852a3b2a2020d159bc94dfeb05ceb95ffb2ac
João Marcos Aires Duarte <joao.duarte@ITBAM.ORG.BR>
2022-01-11 17:28:30 -0400

    Criado o arquivo acessar-telefones.js

---
+++ acessar-telefones.js
@@ -0,0 +1,20 @@
+const cliente = {
+    nome: "João Marcos",
+    idade: 36,
+    cpf: "123543652266",
+    email: "joaomarcos@mail.com",
+    fones: ["5591235498", "5521988743124"]
+}
+
+cliente.fones.forEach(fone => console.log(fone));
+
+
+
+
+
+
+
+
+
+
+
